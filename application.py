d5e61adca74fe207af3bba91ee6258dfea053c02
smailik120 <stasras2020@mail.ru>
2018-04-14 18:15:12 +0600

    new Version with Enter

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, session, request, redirect, url_for
+
 from flask import render_template
 from workwithbd import bd
 import sqlite3
@@ -7,18 +8,42 @@ app = Flask(__name__)
 import sqlite3
 database=bd('dat.sqlite')
 
-@app.route("/",methods=['GET'])
+@app.route("/",methods=['GET','POST'])
 def hello():
-   return render_template("Registration.html")
+    if(request.method=='GET'):
+      #database.delete("users")
+      #database.create("users")
+      #database.add_user("psdaoqsdfodoq","фыылщащзайщзйа","stas","psoao")
+      #database.prin_database()
+      return render_template("Enter.html")
+
+
+
 
 
 @app.route("/lk",methods=['POST'])
 def func1():
     input1=request.form['text']
-    input2=request.form['text1']
-    database.add_user(input1,input2)
-    database.prin_database()
     return render_template("result.html",input1=input1)
+@app.route("/registration",methods=['GET','POST'])
+def func():
+    if(request.method=='GET'):
+
+     return render_template("Registration.html")
+    else:
+        login = request.form['login']
+        password = request.form['password']
+        name = request.form['name']
+        family = request.form['family']
+        Access = database.add_user(login,password,name,family)
+
+        if (Access == True):
+            database.prin_database()
+            return render_template("Enter.html",Access="Вы успешно зарегестрированы")
+
+        else:
+            return render_template("Registration.html",wrong="Такие данные уже существуют или вы ввели неудовлетворяющие условиям логин или пароль")
+
 @app.route("/",methods=['GET','POST'])
 def echo():
     return ""
--- workwithbd.py
+++ workwithbd.py
@@ -5,16 +5,18 @@ class bd():
      self.conn = sqlite3.connect(name)
 
 
- def add_user(self,login,password):
+ def add_user(self,login,password,name,family):
     c = self.conn.cursor()
-    c.execute("SELECT name FROM users WHERE name='"+login+"';")
+    c.execute("SELECT login FROM users WHERE login='"+login+"';")
     log = c.fetchone()
-    if(log is not None):
+    if(log is not None  or len(login)<=6 or len(password)<=6):
         c.close()
+        return False
     else:
-      c.execute("INSERT INTO users (name,password) VALUES (?,?)", ((login), (password)))
+      c.execute("INSERT INTO users (login,password,name,family) VALUES (?,?,?,?)", ((login), (password), (name), (family)))
       self.conn.commit()
       c.close()
+      return True
 
 
  def prin_database(self):
@@ -26,10 +28,18 @@ class bd():
         row=c.fetchone()
     c.close()
 
-     
+
  def clear(self):
     c = self.conn.cursor()
     c.execute("    DELETE FROM users;")
     self.conn.commit()
     c.close()
+ def delete(self,name):
+     c = self.conn.cursor()
+     c.execute("DROP TABLE IF EXISTS " + name+";")
+     self.conn.commit()
+     c.close()
+ def create(self,name):
+     c = self.conn.cursor()
+     c.execute(" CREATE TABLE "+name+" (login TEXT, password TEXT, name TEXT, family TEXT) " + ";")
 
